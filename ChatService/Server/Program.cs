77b2c5852d0572d29d06ea3035c13fba3cf13409
Gurkan Sozdemir <gurkansozdemir@hotmail.com.tr>
2022-08-24 20:16:22 +0300

    Belirtilen porta gelen client isteklerini dinlemesi için server socket oluşturuldu.

--- ChatService/Server/Program.cs
+++ ChatService/Server/Program.cs
@@ -1,7 +1,13 @@
-﻿public class Program
+﻿using Server;
+
+public class Program
 {
     public static void Main(String[] args)
     {
-
+        Console.WriteLine("Sunucu Oluşturuluyor...");
+        ServerSocket serverSocket = new ServerSocket("127.0.0.1", 1234);
+        Console.WriteLine("Sunucu Hazırlandı.");
+        serverSocket.StartListening();
+        Console.ReadLine();
     }
 }
---
+++ ChatService/Server/ServerSocket.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class ServerSocket
+    {
+        private readonly Socket _listenerSocket;
+        private readonly string _host;
+        private readonly int _port;
+        private readonly IPEndPoint _localEndPoint;
+
+        public ServerSocket(string host, int port)
+        {
+            _host = host;
+            _port = port;
+            IPHostEntry ipHost = Dns.GetHostEntry(_host);
+            _listenerSocket = new Socket(ipHost.AddressList[0].AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _localEndPoint = new IPEndPoint(ipHost.AddressList[0], _port);
+        }
+
+        public void StartListening()
+        {
+            _listenerSocket.Bind(_localEndPoint);
+            _listenerSocket.Listen(10);
+            _listenerSocket.BeginAccept(AcceptCallback, _listenerSocket);
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            Socket acceptedSocket = _listenerSocket.EndAccept(ar);
+            _listenerSocket.BeginAccept(AcceptCallback, _listenerSocket);
+        }
+    }
+}
